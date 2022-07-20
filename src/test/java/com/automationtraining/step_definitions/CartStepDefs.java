6328997f245c7ae91ea564a47fdc4a54e3ab42a2
Wainaina Kasyoka <wainaina@Wainainas-MacBook-Pro.local>
2022-07-20 10:21:07 +0300

    initial commit

--- src/test/java/com/automationtraining/step_definitions/CartStepDefs.java
+++ src/test/java/com/automationtraining/step_definitions/CartStepDefs.java
@@ -4,32 +4,32 @@ import com.automationtraining.pages.Add2ItemsToCart;
 import com.automationtraining.pages.Login;
 import com.automationtraining.pages.SignUpAndSignOut;
 import com.automationtraining.utilities.BrowserUtil;
+import com.automationtraining.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
 public class CartStepDefs {
 
-    Login log;
-    SignUpAndSignOut signUp;
-    Add2ItemsToCart add;
+    Add2ItemsToCart add = new Add2ItemsToCart();
+    Actions actions=new Actions(Driver.getDriver());
+    JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
     @Given("user is successfully logged to store")
     public void userIsSuccessfullyLoggedToStore() {
-        signUp = new SignUpAndSignOut();
 
-
-        signUp.goTo();
-        signUp.signinto.click();
-        BrowserUtil.waitFor(4);
-
-        log = new Login();
-        log.login("jamesmaina1@gmail.com","anonymous");
+        add = new Add2ItemsToCart();
+        add.go_To();
         BrowserUtil.waitFor(3);
-
-
-
+        add.signin_to.click();
+        BrowserUtil.waitFor(3);
+        add.loginAccess();
+        BrowserUtil.waitFor(3);
+        add.womenTab.click();
+        BrowserUtil.waitFor(3);
 
 
     }
@@ -39,11 +39,16 @@ public class CartStepDefs {
 
         add = new Add2ItemsToCart();
 
-        add.womenTab.click();
-        BrowserUtil.waitFor(3);
-
         add.item1.click();
         BrowserUtil.waitFor(1);
+
+        actions.moveToElement(add.item1);
+        BrowserUtil.waitFor(1);
+        actions.moveToElement(add.addToCart);
+        BrowserUtil.waitFor(1);
+        add.addToCart.click();
+        BrowserUtil.waitFor(5);
+
         add.continueShopping.click();
         BrowserUtil.waitFor(1);
 
@@ -54,6 +59,11 @@ public class CartStepDefs {
     public void item() {
         add.item2.click();
         BrowserUtil.waitFor(1);
+        actions.moveToElement(add.addToCart2);
+        BrowserUtil.waitFor(1);
+        add.addToCart2.click();
+        BrowserUtil.waitFor(5);
+
     }
 
     @Then("There are two items in your cart message is displayed")
